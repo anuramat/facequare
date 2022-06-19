84db982a06d8c3982ff837bf1ff84fd952c502ca
Arsen Nuramatov <nuramatov@hotmail.com>
2022-06-19 23:16:47 +0300

    all done

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# face tracking virtual webcam output
+# facequare
+a simple face tracking script
+webcam -> this script -> virtual webcam
 
-install requirements, run main.py
+# warning
+besides the requirements.txt, you also need to install some backend for virtual webcam, more info [here](https://pypi.org/project/pyvirtualcam/)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from sqlite3 import adapt
 import cv2
 import pyvirtualcam
 from collections import namedtuple
@@ -6,45 +7,21 @@ import numpy as np
 from multiprocessing import Process, Pipe
 from time import time
 
-# NOTE make it a dataclass?
+# XXX make it a dataclass?
 Rect = namedtuple("Rect", ["x", "y", "w", "h"])
 Dim = namedtuple("Dim", ["h", "w"])
 
 
-def make_center_rect(in_dim, out_dim):
-    out_aspect = out_dim.h / out_dim.w
-
-    h = in_dim.h
-    w = in_dim.w
-
-    center_x = w // 2
-    center_y = h // 2
-
-    new_w = int(h / out_aspect)
-    new_h = int(w * out_aspect)
-
-    if new_w < w:
-        w = new_w
-    elif new_h < h:
-        h = new_h
-
-    x = center_x - w // 2
-    y = center_y - h // 2
-
-    return Rect(x=x, y=y, w=w, h=h)
-
-
 def main(
     cam_index=0,
-    out_dim=Dim(256, 1024),
+    out_dim=Dim(512, 512),
     speed=0.1,
     demo=False,
     offset_ratio=0.3,
     out_fps=30,
-    idle_period=1,
+    idle_period=2,
 ):
 
-    # TODO rewrite to support rectangle output
     frame_num = 0
     last_face_update = float("-inf")
     child_conn, parent_conn = Pipe()
@@ -57,7 +34,7 @@ def main(
         frame = get_frame()
         in_dim = Dim(h=frame.shape[0], w=frame.shape[1])
         parent_conn.send(frame)
-        center_rect = make_center_rect(in_dim, out_dim)
+        center_rect = adapt_rect(in_dim, out_dim, Rect(0, 0, in_dim.w, in_dim.h))
         cur_rect = center_rect
         while True:
             frame_num += 1
@@ -67,7 +44,7 @@ def main(
             if parent_conn.poll():
                 face = parent_conn.recv()
                 if face:
-                    target = face
+                    target = adapt_rect(in_dim, out_dim, face)
                     last_face_update = time()
                 parent_conn.send(frame)
 
@@ -76,13 +53,13 @@ def main(
                 target = center_rect
 
             # smooth step towards target
+            # TODO doesn't exactly respect the aspect ratio
             cur_rect = np.asarray(cur_rect)
             target = np.asarray(target)
             cur_rect = cur_rect + (target - cur_rect) * speed
             cur_rect = Rect(*(int(i) for i in cur_rect))
 
             # zoom in
-            cur_rect = center_rect  # TODO remove
             frame = rect_zoom(frame, cur_rect, out_dim)
 
             # send frame to virtual cam
@@ -104,8 +81,6 @@ def rect_zoom(frame, rect, out_dim):
         rect.x : rect.x + rect.w,
         :,
     ]
-    # TODO respect aspect ratio in case rect collides with border (pad or smth)
-    # or should this logic be where we make the Rect()
     # resize
     frame = cv2.resize(
         cropped_frame, (out_dim.w, out_dim.h), interpolation=cv2.INTER_AREA
@@ -115,7 +90,6 @@ def rect_zoom(frame, rect, out_dim):
 
 
 def find_face_worker(conn, offset_ratio):
-    # NOTE returns rect face, random aspect ratio
     face_cascade_name = cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
     face_cascade = cv2.CascadeClassifier()
     face_cascade.load(face_cascade_name)
@@ -126,7 +100,6 @@ def find_face_worker(conn, offset_ratio):
         faces = face_cascade.detectMultiScale(frame_gray)
         faces = [Rect(*i) for i in faces]
         face = None
-        # TODO remove false xd
         if faces:
             face = max(faces, key=lambda x: x.w * x.h)
             offset = int(offset_ratio * (face.h + face.w) / 2)
@@ -137,6 +110,53 @@ def find_face_worker(conn, offset_ratio):
                 face.h + offset * 2,
             )
         conn.send(face)
+
+
+# XXX make a Rect method?
+def adapt_rect(in_dim, out_dim, rect):
+
+    w = rect.w
+    h = rect.h
+    center_x = rect.x + w // 2
+    center_y = rect.y + h // 2
+    rect_aspect = h / w
+
+    out_aspect = out_dim.h / out_dim.w
+
+    # 1) calculate new_rect.h,w (should fit in in_dim) (cropped image should fully contain rect)
+    # 1a) make an out_dim shaped rectangle, containing "rect"
+    # by changing either h or w so that it matches aspect ratio
+    if out_aspect > rect_aspect:
+        h = int(w * out_aspect)
+    elif out_aspect < rect_aspect:
+        w = int(h / out_aspect)
+
+    # 1b) make this out_dim shaped rectangle fit in in_dim shaped rectangle
+    # by changing h/w proportionally
+    if w > in_dim.w:
+        h = int(h * in_dim.w / w)
+        w = in_dim.w
+    if h > in_dim.h:
+        w = int(w * in_dim.h / h)
+        h = in_dim.h
+
+    # 2) if any of the borders (x, x+w, y, y+h) are out of bounds of [0:h,0:w] => subtract difference from x/y
+    x = center_x - w // 2
+    y = center_y - h // 2
+
+    if x < 0:
+        x = 0
+    elif x + w > in_dim.w:
+        x = in_dim.h - h
+
+    if y < 0:
+        y = 0
+    elif y + h > in_dim.h:
+        y = in_dim.h - h
+
+    result = Rect(x, y, w, h)
+
+    return result
 
 
 class webcam_input:
@@ -157,4 +177,4 @@ class webcam_input:
 
 
 if __name__ == "__main__":
-    main(cam_index=0, demo=True)
+    main()
